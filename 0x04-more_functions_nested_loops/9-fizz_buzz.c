5610cdf6757969392650c04af2ee13e373035d89
SiyabongaMhlongo <skmhlongo24@gmail.com>
2022-06-09 19:21:24 -0700

    Initial commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+
+int p = 100;
+int i;
+
+i = 1;
+while (i <= p)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+
+
