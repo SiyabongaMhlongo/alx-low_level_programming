284d60b245aadc130eb9d5a1e3db4a07d7a33790
SiyabongaMhlongo <skmhlongo24@gmail.com>
2022-06-16 06:39:43 -0700

    Inital commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+include <stdio.h>
 /**
  * main - entry point
  *
@@ -19,4 +19,29 @@ if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz ");
 }
 
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
 
+else if (i % 5 == 0)
+{
+if (i < p)
+printf("Buzz ");
+
+else
+printf("Buzz");
+}
+
+else
+{
+printf("%i ", i);
+}
+
+i++;
+
+
+}
+printf("\n");
+return (0);
+}
